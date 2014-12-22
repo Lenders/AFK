fa2a2b35c75d0b887ee04598f33f57592da0d351
vincent4vx <quatrevieux.vincent@gmail.com>
2014-12-22 12:58:11 +0100

    ADD thumb on userlist

--- app/views/search/userlist.php
+++ app/views/search/userlist.php
@@ -1,6 +1,7 @@
             <ul class="user_list">
             <?php foreach($users as $user):?>
                 <li>
+                    <img class="thumb" src="<?php echo $user['AVATAR'] ? $this->secureUrl('image', 'get', $user['USER_ID'], $user['AVATAR']) : $this->url('resources/images/default.png')?>" alt="Avatar"/>
                     <a class="userlink" href="<?php echo $this->secureUrl('account', 'profile', $user['USER_ID'])?>">
                         <?php echo $user['PSEUDO']?> (<?php echo $user['FIRST_NAME'], ' ', $user['LAST_NAME']?>)
                     </a>
