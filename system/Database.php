78d417e741a8778c54dedacd9f9c7a3c24175448
vincent4vx <quatrevieux.vincent@gmail.com>
2014-12-24 16:11:59 +0100

    FIX utf8 encoding for database

--- system/Database.php
+++ system/Database.php
@@ -47,9 +47,12 @@ class Database extends \PDO{
             $config->pass,
             array(
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
             )
         );
+        
+        
     }
     
     private function generateDSN(){
